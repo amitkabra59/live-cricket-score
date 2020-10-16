0d292fdddb6f518732ac85566a4dffceca793443
Amit Kabra <amit.kabra@byon8.com>
2020-10-16 19:22:00 +0200

    Moving server.js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const serveStatic = require('server-static')
+const serveStatic = require('serve-static')
 const path = require('path')
 
 
@@ -7,7 +7,7 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const PORT = process.env.PORT || '8080'
+const PORT = process.env.PORT || 8080
 app.set("port", PORT)
 app.listen(PORT)
 
